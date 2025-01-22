f9e0c792fc5f94e3a04354162de22438f9e15821
Diego Canales <diego.canales@mongodb.com>
2025-01-22 17:55:15 -0600

    added till section 8- 71

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -1,9 +1,21 @@
 import React, { useState, useEffect } from "react";
 
-export default function TicketForm() {
+export default function TicketForm({dispatch, editingTicket}) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("1");
+
+
+  useEffect(()=> {
+    if(editingTicket){
+        setTitle(editingTicket.title)
+        setDescription(editingTicket.description)
+        setPriority(editingTicket.priority)
+    }else{
+        clearForm()
+    }
+
+  }, [editingTicket])
 
   const priorityLabels = {
     1: "Low",
@@ -19,6 +31,19 @@ export default function TicketForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const ticketDate = {
+        id: editingTicket ? editingTicket.id: new Date().toISOString(),
+        title,
+        description,
+        priority
+    };
+
+    dispatch({
+        type: editingTicket? "UPDATE_TICKET" : "ADD_TICKET",
+        payload: ticketDate
+    })
+
     clearForm();
   };
 
